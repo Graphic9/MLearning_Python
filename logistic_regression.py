41728a98a279272b85507e9b640a4f0ff609ea4a
Graphic9 <l069862171@qq.com>
2017-08-11 11:58:25 +0800

    this is version.3 - logistic regresssion.

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,6 +4,7 @@ import numpy as np
 import sklearn as sk
 import random
 import scipy as sp
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 
 def produce_data(filepath='logistic_regression.txt'):
@@ -44,7 +45,7 @@ def gradAscent(X_Train,Y_Train,theata,alpha,n_iter):
     m = X_Train.shape[0];
     J = np.zeros(n_iter).reshape(n_iter,1);
     for i in range(n_iter):
-        index = int(np.round(np.random.random(1)[0]*(m-1)));
+        index = i%(m-1);
         x = X_Train[index,:].reshape(1,X_Train.shape[1]);
         y = Y_Train[index,0].reshape(1,1);
         J[i,0] = predict(X_Train,Y_Train,theata)[1];
@@ -62,6 +63,38 @@ def loadDataSet():
         # labelMat.append()
     return dataMat;
 
+
+# 分析数据，画出决策边界
+def plotBestFit(wei, dataMatrix, labelMat):
+    weights = wei;  # 将矩阵wei转化为list
+    dataArr = dataMatrix;  # 将矩阵转化为数组
+    n = dataMatrix.shape[0];
+    xcord1 = [];
+    ycord1 = []
+    xcord2 = [];
+    ycord2 = []
+
+    for i in range(n):
+        if int(labelMat[i]) >= 0.5:
+            xcord1.append(dataArr[i, 0])
+            ycord1.append(dataArr[i, 1])
+        else:
+            xcord2.append(dataArr[i, 0])
+            ycord2.append(dataArr[i, 1])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c="green")
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[2] - weights[0] * x) / weights[1]
+    ax.plot(x, y)
+    plt.xlabel("x1")  # X轴的标签
+    plt.ylabel("x2")  # Y轴的标签
+    plt.show()
+
+
+
 if __name__ == '__main__':
     #produce_data();
     # data = np.loadtxt('logistic_regression.txt', delimiter='    ');
@@ -77,11 +110,20 @@ if __name__ == '__main__':
 
     theata = np.zeros(X_Train.shape[1]).reshape(3,1);
     alpha = 0.001;
-    n_iter  = 1000;
+    n_iter  = 1000000;
     [theata, J] = gradAscent(X_Train, Y_Train, theata, alpha, n_iter)
     [Y_Pre,loss] = predict(X_Test,Y_Test,theata);
 
+    lr = LogisticRegression(solver='sag',max_iter=10000,fit_intercept=True,intercept_scaling = 1);
+    lr.fit(X_Train[:,0:2], Y_Train[:,0]);
+    # pred_test = lr.predict(X_Test[:,0:2]).reshape(34,1);
+
+    plotBestFit(theata, X_Test, Y_Test);
+
+    [pred_test,loss1] = predict(X_Test,Y_Test,np.c_[lr.coef_,lr.intercept_].T);
     print('the last loss is %lf' % loss);
+    print('the last loss is %lf' % loss1);
+    print('the max_iterator is %d' % lr.max_iter);
 
     # plt.xlim(-10,10);
     # plt.ylim(-1,1);
